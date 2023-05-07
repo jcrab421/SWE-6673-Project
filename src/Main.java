14efc3dd886105493fe07764d341694890aec107
docktor-Q <77511282+docktor-Q@users.noreply.github.com>
2023-05-07 16:59:53 -0400

    changed folder path

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,7 @@ public class Main {
 		//create DataProcessor object to process coverage data.
 		DataProcessor resultProcessor = new DataProcessor();
 	
-		String coveragefolder = "C:\\Users\\Fr3dy Q\\Documents\\Grad school work_start2018" +
-                "\\SWE 6673_Software Testing Verification_Spring2023" +
-                "\\SWE-6673-Project\\Swe6673Project\\Swe6673TestingProject\\CoverageData";
+		String coveragefolder = ".\\SWE-6673-Project\\CoverageData";
 		
 		String folderPath = coveragefolder; 
 		resultProcessor.processdata(folderPath);
